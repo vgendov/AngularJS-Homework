4f7f98dcc9f78d3b930432007e64c0685442d144
vgendov <vegeta_m@abv.bg>
2015-01-13 17:29:32 +0200

    Finished (without css)

--- Char Select/controller/controller.js
+++ Char Select/controller/controller.js
@@ -1,17 +1,20 @@
 app.controller('Main', function($scope, $sce) {
-	var data = [];
+	var data = [],
+		classArray = []
+		styleArray = [];
 
 	var firstChar = {
 		name: 'Valoris',
 		type: 'Barbarian',
 		pictureUrl: 'http://stuffershack.com/wp-content/uploads/2013/02/barbarian1.jpg',
+		style: 'Mele',
 		skills: [{
 			name: 'Slam',
-			damage: '230',
+			effect: '230 HP Damage',
 			cooldown: '5s'
 		}, {
 			name: 'Hammer',
-			damage: '380',
+			effect: '380 HP Damage',
 			cooldown: '10s'
 		}]
 	};
@@ -19,20 +22,38 @@ app.controller('Main', function($scope, $sce) {
 	var secondChar = {
 		name: 'Brom',
 		type: 'Templar',
-		pictureUrl: 'http://media-cache-ec0.pinimg.com/736x/14/93/e4/1493e422354d5e7ba9c73dd19b042c2f.jpg',
+		pictureUrl: 'http://dragonmountbooks.files.wordpress.com/2012/07/cavaleiro-templario.jpg',
+		style: 'Mele',
 		skills: [{
 			name: 'Heal',
-			damage: '150',
+			effect: '+150 HP Life',
 			cooldown: '15s'
 		}, {
 			name: 'Shield Slam',
-			damage: '100',
+			effect: '100 Damage + Stun for 3s',
 			cooldown: '5s'
+		}]
+	};
+
+	var thirdChar = {
+		name: 'Leona',
+		type: 'Amazon',
+		pictureUrl: 'http://talesfromthefireside.com/gallerynpcs/ninaran%20(0).jpg',
+		style: 'Range',
+		skills: [{
+			name: 'Double Shot',
+			effect: '410 HP Damage',
+			cooldown: '15s'
+		}, {
+			name: 'Slow Down',
+			effect: 'Slows enemy movement for 5s',
+			cooldown: '8s'
 		}]
 	};
 
 	data.push(firstChar);
 	data.push(secondChar);
+	data.push(thirdChar);
 	$scope.data = data;
 
 	$scope.makeUrl = function(url) {
@@ -40,11 +61,39 @@ app.controller('Main', function($scope, $sce) {
 	}
 
 	var sortFunc = function(predicate) {
-		return function(item) {
-			if(predicate == ''){
-
+		return function(character) {
+			if(predicate == 'type'){
+				return character.type;
+			}
+			if(predicate == 'style'){
+				return character.style;
+			}
+			if(predicate == 'name'){
+				return character.name;
 			}
 		}
 	}
+
+	function makeClassList() {
+        data.forEach(function(elem) {
+            if (classArray.indexOf(elem.type) === -1) {
+                classArray.push(elem.type);
+            };
+        });
+    }
+    makeClassList();
+
+    function makeStyleList() {
+        data.forEach(function(elem) {
+            if (styleArray.indexOf(elem.style) === -1) {
+                styleArray.push(elem.style);
+            };
+        });
+    }
+    makeStyleList();
+
+    $scope.styles = styleArray;
+    $scope.classes = classArray;
+	$scope.sortFunc = sortFunc;
 
 });
